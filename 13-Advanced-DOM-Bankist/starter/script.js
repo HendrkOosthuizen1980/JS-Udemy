9a1bec5f540d2d9cad00529433b7f6409beb5692
Hendrik <hendrik@thecodeblock.net>
2022-10-31 21:39:15 +0200

    add select create delete elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -21,9 +21,6 @@ const closeModal = function () {
 
 btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
-
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -32,3 +29,42 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//Selecting Elements
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+//Creating and inserting elements
+
+const message = document.createElement('div'); //element is not in DOM as yet
+message.classList.add('cookie-message'); //adds class to element
+//message.textContent = "We use cookies for improved functionality and analytics."; //inserts text
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class = "btn btn--close-cookie">Got it!</button>';
+
+//header.prepend(message);
+header.append(message);
+//header.append(message.cloneNode(true));
+
+//header.before(message);
+//header.after(message);
+
+//Delete Elements
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
