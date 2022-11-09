9b0660c63da6bfb78072794e15e1ac2c3315f327
Hendrik <hendrik@thecodeblock.net>
2022-11-09 21:01:19 +0200

    complete style, attributes and class sections

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -71,5 +71,45 @@ document
 
 //Style
 
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
+
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// //attributes
+
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.getAttribute('src'));
+// console.log(logo.className);
+
+// logo.alt = 'Beautiful minimalist logo';
+
+// //attribute non-standard
+// console.log(logo.designer);
+// console.log(logo.getAttribute('designer'));
+// logo.setAttribute('company', 'Bankist');
+
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
+
+// //Data attributes
+
+// console.log(logo.dataset.versionNumber);
+
+// //Classes
+// logo.classList.add('c');
+// logo.classList.remove('j');
+// logo.classList.toggle('k');
+// logo.classList.contains('l');
+
+// //Don't use
+// logo.className = 'Jonas';
